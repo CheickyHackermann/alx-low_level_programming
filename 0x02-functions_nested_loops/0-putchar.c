24554cb3d815b9a721afea3f70d8fcec32242ef5
CheickyHackermann <konecheickoumar101@gmail.com>
2022-07-05 09:33:31 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** main - printsn _putchar, followed with a new line
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return(0);
+}
